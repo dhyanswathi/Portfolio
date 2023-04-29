b882a3dbcb22c4339ba4e1a52b3a7e640e591a76
swathi <swathysidharthan@gmail.com>
2023-04-29 10:52:57 +0200

    add project data

--- portfolio/src/Data/projects.ts
+++ portfolio/src/Data/projects.ts
@@ -1,1 +1,31 @@
-export {}
+export const projects = [
+    {
+        title: "Toy World",
+        subtitle: "ASP.NET WebAPI, React, Javascript, SQL Server, Entity framework, Bootstrap",
+        description:
+          "An app I have developed for swapping pre-loved toys",
+        image: "../Pictures/toyworld.png",
+        link: "https://github.com/dhyanswathi/ToyWorld",
+    },
+    {
+        title: "Tiny blog",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A blog that fetch posts from an external Api and displays according to category",
+        image: "../Pictures/toyworld.png",
+        link: "https://tinyblog-ts-react.netlify.app"
+    },
+    {
+        title: "Resume Builder",
+        subtitle: "Asp.Net WebAPI, React, Javascript, HTML, CSS, SQL Server, Entity framework, Azure",
+        description: "An application that helps you create a resume in different templates. This was the final project by our group during salt bootcamp.",
+        image: "../Pictures/toyworld.png",
+        link: "https://github.com/dhyanswathi/ToyWorld"
+    },
+    {
+        title: "Flight Finder",
+        subtitle: "Asp.Net WebAPI, ",
+        description: "finding flights",
+        image: "../Pictures/toyworld.png",
+        link: "https://github.com/dhyanswathi/ToyWorld"
+    }
+]
