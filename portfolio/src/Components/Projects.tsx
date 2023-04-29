4f5beab394e840507370eb02a5ea3d47b2fd7ac4
swathi <swathysidharthan@gmail.com>
2023-04-29 11:18:42 +0200

    add project as cards

--- portfolio/src/Components/Projects.tsx
+++ portfolio/src/Components/Projects.tsx
@@ -1,18 +1,62 @@
 import React from "react";
-import { MantineProvider, Grid } from '@mantine/core';
+import {Card, Image, Text, Group, Grid, Button } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 import Nav from "./Navbar";
+import { projects } from "../Data/projects";
 
 function Projects() {
     return (
         <>
-        <MantineProvider>
       <Grid>
       <Grid.Col span={3}><Nav></Nav></Grid.Col>
       <Grid.Col span={9}>
-      projects
+      <section id="projects" className="projects">
+      <div className="group">
+          <h1 className="ss">
+            Apps I've Built
+          </h1>
+          <p className="des">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
+            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
+            fuga dolore.
+          </p>
+        </div>
+        <Carousel
+      withIndicators
+      height={400}
+      slideSize="33.333333%"
+      slideGap="md"
+      loop
+      align="start"
+      slidesToScroll={3}
+    >
+          {projects.map((project) => (<Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+                <Image
+                  alt="gallery"
+                  height={160} width={90}
+                  src={project.image}
+                /></Card.Section>
+                <Group position="apart" mt="md" mb="xs">
+                <Text weight={500}>
+                    {project.title}
+                  </Text>
+                  </Group>
+                  <Text weight={300}>
+                    {project.subtitle}
+                  </Text>
+                  
+                  <Text size="sm" color="dimmed">{project.description}</Text>
+                  <a
+              href={project.link}
+              key={project.image}
+              className="ss"><Button>Go to Project</Button>
+            </a></Card>
+          ))}
+        </Carousel>
+    </section>
       </Grid.Col>
       </Grid>
-    </MantineProvider>
         </>
     )
 }
