a63af5b2a1da670b8d0300906569f755dc904931
swathi <swathysidharthan@gmail.com>
2023-05-03 19:19:34 +0200

    add email service

--- portfolio/src/Components/Contact.tsx
+++ portfolio/src/Components/Contact.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../Styles/Contact.css';
 
 function Contact() {
+
+  const form = useRef<null | HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement | null>) => {
+    e.preventDefault();
+
+    const currentForm = form.current;
+    if (currentForm == null) return;
+
+    emailjs.sendForm('service_93flvy6', 'template_nwt7xv8', currentForm.current, 'NSV6aTQjw2wXfm8vQ')
+      .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+  };
     return (
         <>
           <section className="contact">
@@ -9,7 +26,15 @@ function Contact() {
               Swathi Sidharthan
             </article>
             <article className="email">
-
+            <form ref={form} onSubmit={sendEmail}>
+              <label>Name</label>
+              <input type="text" name="user_name" />
+              <label>Email</label>
+              <input type="email" name="user_email" />
+              <label>Message</label>
+              <textarea name="message" />
+              <input type="submit" value="Send" />
+            </form>
             </article>
           </section>
         </>
