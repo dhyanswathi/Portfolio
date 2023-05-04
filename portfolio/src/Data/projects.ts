e6934e8169a8c8044ef6ecadf2e4c7126abf3e38
swathi <swathysidharthan@gmail.com>
2023-05-04 10:12:35 +0200

    import images for project

--- portfolio/src/Data/projects.ts
+++ portfolio/src/Data/projects.ts
@@ -1,4 +1,13 @@
 import toyworld from '../Pictures/toyworld.png';
+import flight from '../Pictures/flight.png';
+import library from '../Pictures/library.png';
+import milk from '../Pictures/milk.png';
+import portfolio from '../Pictures/portfolio.png';
+import puppies from '../Pictures/puppies.png';
+import resume from '../Pictures/resume.png';
+import saltazon from '../Pictures/saltazon.png';
+import tiny from '../Pictures/tiny.png';
+
 
 export const projects = [
     {
@@ -10,24 +19,60 @@ export const projects = [
         link: "https://github.com/dhyanswathi/ToyWorld",
     },
     {
-        title: "Tiny blog",
+        title: "Keep Scribbling",
         subtitle: "React, Typescript, Bootstrap",
-        description: "A blog that fetch posts from an external Api and displays according to category",
-        image: "portfolio/src/Pictures/toyworld.png",
+        description: "A react app for tiny blogs that fetch posts from an external Api and displays according to category",
+        image: tiny,
         link: "https://tinyblog-ts-react.netlify.app"
     },
     {
         title: "Resume Builder",
         subtitle: "Asp.Net WebAPI, React, Javascript, HTML, CSS, SQL Server, Entity framework, Azure",
         description: "An application that helps you create a resume in different templates.",
-        image: "../Pictures/toyworld.png",
+        image: resume,
         link: "https://github.com/dhyanswathi/ToyWorld"
     },
     {
         title: "Flight Finder API",
         subtitle: "Asp.Net WebAPI, SQL Server, Entity Framework, Authentication & Authorization with JWT Tokens",
         description: "A flight booking API providing all possible flights and their corresponding information, a specific flight, connecting flights to a specific flight, etc.",
-        image: "../Pictures/toyworld.png",
+        image: flight,
         link: "https://github.com/dhyanswathi/Flight-finder"
+    },
+    {
+        title: "Saltazon",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A react app - blog that fetch posts from an external Api and displays according to category",
+        image: saltazon,
+        link: "https://tinyblog-ts-react.netlify.app"
+    },
+    {
+        title: "Milk App",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A react app - blog that fetch posts from an external Api and displays according to category",
+        image: milk,
+        link: "https://tinyblog-ts-react.netlify.app"
+    },
+    {
+        title: "Library MVC",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A react app - blog that fetch posts from an external Api and displays according to category",
+        image: library,
+        link: "https://tinyblog-ts-react.netlify.app"
+    },
+    {
+        title: "Puppy Adoption",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A react app - blog that fetch posts from an external Api and displays according to category",
+        image: puppies,
+        link: "https://tinyblog-ts-react.netlify.app"
+    },
+    {
+        title: "Portfolio",
+        subtitle: "React, Typescript, Bootstrap",
+        description: "A react app - blog that fetch posts from an external Api and displays according to category",
+        image: portfolio,
+        link: "https://tinyblog-ts-react.netlify.app"
     }
+
 ]
